c8a9c847ef508ec7f350ed28a97eea34c0a3873a
Student Annelieke van der Kwast <akwast9872@student.landstede.nl>
2022-03-14 15:35:47 +0100

    6.1 de route nog even gefixt

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/new', function () {
     return view('new');
 });
 
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('test');
 });
 

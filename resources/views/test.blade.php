8ac1abe0ab373d4b7e3887f3713f39f680fb9986
Student Annelieke van der Kwast <akwast9872@student.landstede.nl>
2022-03-14 11:36:45 +0100

    5.0 test blade gemaakt om dingen te testen en de footer gemaakt

---
+++ resources/views/test.blade.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>De website</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+</head>
+<body style="overflow-x: hidden;">
+{{--test--}}
+<nav class="navbar navbar" style="margin-bottom: 0.5vw;
+height: 7rem;
+z-index: 2;">
+    <div class="container-fluid" style="justify-content: flex-end;">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" style="position: absolute;
+float: left;
+left: 1vw;
+top: 0.3vw;"><img src="assetsTestCase/logo-clr.svg"></a>
+        </div>
+        <ul class="nav navbar-nav" style="flex-direction: row;
+font-size: 15px;
+margin-top: 0.5vw;">
+            <li class="dropdown"><a class="dropdown-toggle topNavText" data-toggle="dropdown" href="#">DIENSTEN</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle topNavText" data-toggle="dropdown" href="#">CASE STUDIES</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li class="dropdown"><a class="dropdown-toggle topNavText" data-toggle="dropdown" href="#">MARKTEN</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li><a href="#" class="topNavText">OVER RBK</a></li>
+
+            <div style="display: flex;
+align-items: center;
+margin-top: 0.5rem;
+height: 4rem;">
+                <a href="#" class="myButton" style="justify-content: center;
+align-items: center;
+display: flex;margin-left: 0.5vw;">CONTACT</a>
+
+                <img src="assetsTestCase/nederland.png" style="width: 25px;
+height: 20px;margin-left: 1vw">
+            </div>
+        </ul>
+    </div>
+</nav>
+
+{{--<div style="background-image: url(/assetsTestCase/header-img.jpg);width: 100vw;height: 90vh;">    IK had eerst dit geprobeerd--}}
+<div>
+    <img src="assetsTestCase/header-img.jpg" style="width: 100vw;height: 90vh; position: relative">
+    <div style="width: 67vw;
+position: absolute;
+z-index: 2;
+top: 11vw;">
+        <p style="background: white;
+padding: 2vw;
+font-size: 2.8vw;
+padding-left: 4vw; margin: 0">Het Logo is dé partner op het gebied van <b>bouw</b> en <b>verbouwing,
+                milieuvraagstukken</b> en <b>automatisering</b> voor <b>foodindustrie.</b></p>
+
+        {{--Ik weet ook even niet wat hier gebeurdt--}}
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <div class="container" style="padding: 0.8vw;">
+            <a href="#" data-toggle="popover" data-content="Ik ga nergens heen ( ͡❛ ͜ʖ ͡❛)" style="color: white;
+font-weight: bold;
+font-size: 1vw;
+background: #f0761d;
+padding: 1vw; text-decoration: none">Belafspraak inplannen ›</a>
+        </div>
+
+        <script>
+            $(document).ready(function () {
+                $('[data-toggle="popover"]').popover();
+            });
+        </script>
+    </div>
+    {{--        Hier moeten die blauwe dingen en groenen dingen nog--}}
+</div>
+
+{{-----------------------------------------------------------------------------------------------------------------------------------------------------------------------}}
+<div style="margin-right: 10vw;
+margin-left: 10vw;">
+    {{----}}
+    {{----}}
+
+    <div>
+        <div style="
+display: flex;
+align-content: center;margin-top: 4rem;">
+            <h2><b>Actueel</b></h2>
+        </div>
+        <div style="display: flex; justify-content: space-between;">
+            <div class="wrapperPicture">
+                <img src="assetsTestCase/beyond-meat.jpg" style="width: 37vw;
+">
+                <div class="textPictureDiv">
+                    <p><b>Het Logo neemt Adviesburo Bureay over</b></p>
+                    <p>19-01-2021</p>
+                </div>
+                <div style="background: #f0761d;
+width: 3vw;
+position: relative;
+top: -10.5vw;
+left: 2vw;">
+                    <p style="color: white;
+font-size: 1.3vw;
+padding: 1vw;
+background: #f0761d;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 3vw;
+height: 6vh;">›</p>
+                </div>
+            </div>
+            {{--        het andere plaatje--}}
+            <div class="wrapperPicture">
+                <img src="assetsTestCase/zandbergen.jpg" style="width: 37vw;
+">
+                <div class="textPictureDiv">
+                    <p><b>Het Logo neemt Adviesburo Bureay over</b></p>
+                    <p>19-01-2021</p>
+                </div>
+                <div style="background: #f0761d;
+width: 3vw;
+position: relative;
+top: -10.5vw;
+left: 2vw;">
+                    <p style="color: white;
+font-size: 1.3vw;
+padding: 1vw;
+background: #f0761d;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 3vw;
+height: 6vh;">›</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-----------------------------------------------------------------------------------------------------------------------------------------------------------------------}}
+
+    <div style="display: flex;
+justify-content: flex-end;
+margin-top: 14rem; position: relative">
+        <div style="position: absolute;
+left: 0rem;
+top: 10rem;">
+            <div style="
+z-index: 2;
+width: 38vw;
+left: 13.5vw;
+background: white;
+top: 7rem;
+padding: 1.5vw;
+padding-left: 0vw;
+padding-right: 3.5vw;
+padding-bottom: 1vw;">
+                <p style="font-size: 2.1vw">
+                    <b>
+                        Het Logo is al 40 jaar de specialist in de voedingsmiddelenindustrie
+                    </b>
+                </p>
+                <p style="font-size: 1.2vw;
+font-weight: lighter;">
+                    Het Logo levert oplossingen op het gebied van automatisering,
+                    milieu advies, masterplanning en logistieke inrichtingen. Dit maakt
+                    Het Logo één van de meest complete, innovatieve en
+                    vooraanstaande organisaties op het gebied van
+                    rendementsverhogende oplossingen binnen de foodsector.
+                </p>
+
+            </div>
+            <div style="
+left: 14.5vw;
+top: 25.3rem;">
+                <p style="background: #f0761d;
+padding: 1vw;
+color: white;
+font-weight: bold; width: fit-content;">Meer over Het Logo   <b style="font-size: 1vw;">›</b></p>
+            </div>
+        </div>
+        <img src="assetsTestCase/robot.jpg" style="width: 47vw;">
+
+    </div>
+
+    {{--Opdrachtgevers---------------------------------------------------------------------------------------------------------------------------------------------------------------------}}
+
+    <div style="
+display: flex;
+align-content: center;
+margin-top: 3.5vw;
+margin-bottom: 1vw;">
+        <h2><b>Trots op onze opdrachtgevers</b></h2>
+    </div>
+
+    <div class="container" style="
+align-items: center;
+display: flex; width: 100%; padding: 0;
+justify-content: center;">
+        <div class="row justify-content-md-center" style="width: 100%; padding: 0">
+            <div class="opdrachtGeverCOL col" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/hoogvliet.png" class="hoogvliet">
+                </div>
+            </div>
+            <div class="opdrachtGeverCOL col" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/vitelco.png" class="vitelco">
+                </div>
+            </div>
+            <div class="col opdrachtGeverCOL" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/encko.png" class="encko">
+                </div>
+            </div>
+            <div class="col opdrachtGeverCOL" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/hoogvliet.png" class="hoogvliet">
+                </div>
+            </div>
+            <div class="col opdrachtGeverCOL" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/vitelco.png" class="vitelco">
+                </div>
+            </div>
+            <div class="col opdrachtGeverCOL" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/encko.png" class="encko">
+                </div>
+            </div>
+            <div class="col opdrachtGeverCOL" style="padding: 0">
+                <div class="opdrachtGevers">
+                    <img src="assetsTestCase/hoogvliet.png" class="hoogvliet">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <p style="padding: 1vw;
+background: #f0761d;
+position: absolute;
+color: white;
+margin-top: 1vw;">
+            <b>Bekijk de case studies   ›</b>
+        </p>
+    </div>
+
+    {{----}}
+    {{----}}
+</div>
+{{--de footer---------------------------------------------------------------------------------------------------------------------------------------------------------------------}}
+
+<footer style="margin-top: 12rem;
+background: #2f7df4; padding-left: 10vw;
+padding-right: 10vw; padding-top: 7vw">
+    <div class="container" style="padding: 0;
+margin: 0;
+margin-right: 1vw;
+margin-left: 1vw;width: 100%;">
+        <div class="row">
+            <div class="col">
+                <img src="assetsTestCase/logo-white.svg">
+                <p style="color: white">
+                    <br>
+                    <br>
+                    Keulenstraat 12
+                    <br>
+                    7418 ET Deventer
+                    <br>
+                    <br>
+                    T. +31(0)570 820 210
+                    <br>
+                    M.info@hoogdesign.nl
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                </p>
+
+                <div class="row" style="padding: 1vw;
+padding-left: 0.5vw;">
+                    <div class="col">
+                        <img src="assetsTestCase/linkedin-2-24.png">
+                    </div>
+                    <div class="col">
+                        <img src="assetsTestCase/twitter-24.png">
+                    </div>
+                    <div class="col">
+                        <img src="assetsTestCase/facebook-7-24.png">
+                    </div>
+                    <div class="col">
+                        {{--                        <img src="assetsTestCase/facebook-7-24.png">--}}
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <p style="color: white">
+                    <b>Project diensten</b>
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                </p>
+            </div>
+            <div class="col">
+                <p style="color: white">
+                    <b>Project automatisering</b>
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br><br>
+                    Submenu item
+                    <br>
+                </p>
+            </div>
+            <div class="col">
+{{--                deze moet leeg blijven--}}
+            </div>
+            <div class="col">
+                <p style="color: white">
+                    <b>Service desk (24/7)</b>
+                    <br><br>
+                    +31(0)0570 820 210
+                    <br><br>
+                    servicedesk@hoogdesign.nl
+                    <br><br>
+                    TeamViewer
+
+                </p>
+            </div>
+        </div>
+    </div>
+
+</footer>
+</body>
+</html>
